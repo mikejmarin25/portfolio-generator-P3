beeebc77e34187d2c463978585dc4d344c985321
mikejmarin25 <105763252+mikejmarin25@users.noreply.github.com>
2022-09-20 15:30:07 -0400

    app.js

--- app.js
+++ app.js
@@ -1,5 +1,33 @@
-const addNums = (numOne, numTwo) => {
-    console.log(numOne, numTwo);
-    return numOne + numTwo;
-  };
+const fs = require('fs');
+const generatePage = require('./src/page-template');
+
+const profileDataArgs = process.argv.slice(2);
+
+console.log(profileDataArgs);
+
+const [name, github] = profileDataArgs;
+
+console.log(name, github);
+
+const pageHTML = generatePage(name, github);
+
+fs.writeFile('./index.html', pageHTML, err => {
+  if (err) throw err;
+
+  console.log('Portfolio complete! Check out index.html to see the output!');
+
+
+  promptUser()
   
+  .then(promptProject)
+  .then(portfolioData => {
+    const pageHTML = generatePage(portfolioData);
+
+    // fs.writeFile('./index.html', pageHTML, err => {
+    //   if (err) throw new Error(err);
+
+    //   console.log('Page created! Check out index.html in this directory to see it!');
+    // });
+  });
+
+  });
